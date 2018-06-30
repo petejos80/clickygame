b5b7d215fa2fb43ffcf01d06c5a98159fa55a3e6
petejos80 <petejos80@gmail.com>
2018-06-30 00:05:45 -0500

    Configuring map for images

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+import React from "react";
+import ImageCard from "./components/ImageCard";
+import Wrapper from "./components/Wrapper";
+import cards from "./cards.json";
+import "./App.css";
+
+const App = () => (
+  <Wrapper>
+    <h1 className="title">Cards List</h1>
+    <ImageCard
+      image={cards[0].image}
+    />
+    <ImageCard
+      image={cards[1].image}
+    />
+    <ImageCard
+      image={cards[2].image}
+    />
+  </Wrapper>
+);
 
 export default App;
+
