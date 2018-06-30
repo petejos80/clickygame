b70e99b1d85f4a329b5cc2ae743656f818bb7053
petejos80 <petejos80@gmail.com>
2018-06-30 00:46:01 -0500

    Working image map

--- src/App.js
+++ src/App.js
@@ -20,26 +20,33 @@
 
 // export default App;
 
-import React from "react";
+
+
+
+import React, { Component } from "react";
 import ImageCard from "./components/ImageCard";
 import Wrapper from "./components/Wrapper";
 import cards from "./cards.json";
 import "./App.css";
 
-const App = () => (
-  <Wrapper>
-    <h1 className="title">Cards List</h1>
-    <ImageCard
-      image={cards[0].image}
-    />
-    <ImageCard
-      image={cards[1].image}
-    />
-    <ImageCard
-      image={cards[2].image}
-    />
-  </Wrapper>
-);
+class App extends Component {
+  // Setting this.state.cards to the cards json array
+  state = {
+    cards
+  };
+
+  // Map over this.state.cards and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        {this.state.cards.map(card => (
+          <ImageCard
+            image={card.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+}
 
 export default App;
-
