420a7a3c21f28aae43cb2b5cf828a0835638b907
petejos80 <petejos80@gmail.com>
2018-07-11 14:11:34 -0500

    Formatting for Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = props => (
     <nav className="navbar">
         <div className="container">
         <h1>Clicky Game</h1>
-        <p className="instructions">Click on an image to earn points, but don't click on any more than once!</p>
+            <p className="instructions">Click on an image to earn points, but don't click on any more than once!</p>
             <span className="navbar-text">
                 Score <span className="badge badge-pill badge-secondary">{props.score}</span> | Top Score <span className="badge badge-pill badge-secondary">{props.highscore}</span>
             </span>
